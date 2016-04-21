14de3a15d84291b285ab9efc7fb4d1f9d6a68d04
Markus Vieth <mavieth@students.uni-mainz.de>
2016-04-21 19:38:36 +0200

    Nummer 5

---
+++ src/DecisionTree.java
@@ -0,0 +1,146 @@
+import org.kramerlab.teaching.ml.datasets.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Markus Vieth on 21.04.2016.
+ */
+public class DecisionTree {
+
+    private Instance[] data;
+    private Dataset dataset;
+
+    public DecisionTree() {
+    }
+
+    public DecisionTree(String path) {
+        try {
+            this.loadArff(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DecisionTree(File file) {
+        try {
+            this.loadArff(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadArff(String path) throws Exception {
+        File file = new File(path);
+        this.loadArff(file);
+    }
+
+    public void loadArff(File file) throws Exception {
+        this.dataset = new Dataset();
+        dataset.load(file);
+        this.data = new Instance[dataset.getNumberOfInstances()];
+
+        for (int i = 0; i < data.length; i++) {
+            this.data[i] = dataset.getInstance(i);
+        }
+    }
+
+    public double informationGain(Attribute attribute, int... indices) {
+        List<Attribute> attributes = dataset.getAttributes();
+        Attribute classAttr = attributes.get(attributes.size() - 1);
+        NominalAttribute attr = (NominalAttribute) attribute;
+        double gain = calculateEntropy((NominalAttribute)classAttr,
+                indices);
+        for (int v = 0; v < attr.getNumberOfValues(); v++) {
+            List<Integer> subIndices = new ArrayList<>();
+            for (int i : indices) {
+                Instance instance = data[i];
+                NominalValue value = (NominalValue)instance.getValue(attr);
+                if (attr.getValue(v).equals(value)) {
+                    subIndices.add(i);
+                }
+            }
+            double entropy = calculateEntropy((NominalAttribute)classAttr,
+                    subIndices);
+            gain -= entropy * ((double)subIndices.size())/((double)indices.length);
+        }
+
+        return gain;
+    }
+
+    private double calculateEntropy(NominalAttribute classAttr, int... indices
+    ) {
+        int[] values = new int[classAttr.getNumberOfValues()];
+        for (int v = 0; v < classAttr.getNumberOfValues(); v++) {
+            values[v] = 0;
+            for(int i : indices) {
+                Instance instance = data[i];
+                NominalValue value = (NominalValue)instance.getValue(classAttr);
+                NominalValue classValue = classAttr.getValue(v);
+                if (classValue.equals(value)) {
+                    values[v]++;
+                }
+            }
+        }
+        return calculateEntropy(values);
+    }
+
+    private double calculateEntropy(NominalAttribute classAttr, List<Integer>
+            indices
+    ) {
+        int[] values = new int[classAttr.getNumberOfValues()];
+        for (int v = 0; v < classAttr.getNumberOfValues(); v++) {
+            values[v] = 0;
+            for(int i : indices) {
+                Instance instance = data[i];
+                NominalValue value = (NominalValue)instance.getValue(classAttr);
+                NominalValue classValue = classAttr.getValue(v);
+                if (classValue.equals(value)) {
+                    values[v]++;
+                }
+            }
+        }
+        return calculateEntropy(values);
+    }
+
+    private double calculateEntropy(int[] values) {
+        double sum = 0;
+        for (int i : values) {
+            sum += i;
+        }
+        double entropy = 0.0;
+
+        for (int value : values) {
+            double p = value/sum;
+            entropy -= p * log2(p);
+        }
+
+        return entropy;
+    }
+
+    private double log2(double a) {
+        if ( Double.compare(0.0, Math.abs(a)) == 0 )
+            return 0;
+        return Math.log(a) / Math.log(2);
+    }
+
+    private void testPrint() {
+        int[] indices = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            indices[i] = i;
+        }
+        for (Attribute attr : this.dataset.getAttributes()) {
+            System.out.print("Attribute " + attr.getName());
+            System.out.print(" has an InformationGain of " + informationGain
+                    (attr, indices));
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        DecisionTree dt = new DecisionTree("res/weather.nominal.arff");
+        dt.testPrint();
+    }
+}
